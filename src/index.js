1d892616cd549c56f3e9b7783e81e7c008b7a63b
Rohan Bhaskar <46901880+rohanbhaskar99@users.noreply.github.com>
2019-08-30 15:59:30 +0530

    Added switcher for the themes using hooks -updated a bit of styling for the toggle button

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import useTheme from "./hooks/useTheme";
+import "./styles.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+	
+	const { theme, toggleTheme } = useTheme();
+	return (
+		<div className={`App ${theme}`} style={{height:'110vh'}}>
+				<button className="toggle-button" type="button" onClick={toggleTheme}>
+					<div className="toggle-text">Toggle theme</div>
+				</button>
+		</div>
+	);
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
